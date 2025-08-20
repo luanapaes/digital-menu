d5cb3fe7f36d8bbfb3fd8d10bd994f8190a3cb97
Luana Paes <luanafranca598@gmail.com>
2025-08-20 01:06:29 -0300

    Add array categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { BannerComponent } from '../../shared/components/banner/banner.component';
+import { CategoryButtonComponent } from '../../shared/components/category-button/category-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent],
+  imports: [HeaderComponent, BannerComponent, CategoryButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  categories = [
+    "Café da Manhã", "Almoço", "Salada", "Sobremesa", 
+    "Pizza", "Hambúrguer", "Massa",
+    "Pizza", "Hambúrguer", "Massa",
+  ];
 }
