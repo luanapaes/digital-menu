fcb47b741b0485e1113550409faec1a022cca2b6
Luana Paes <luanafranca598@gmail.com>
2025-08-20 15:23:07 -0300

    Adiciona exibição dos pratos com possibilidade de buscar prato pelo nome*

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { BannerComponent } from '../../shared/components/banner/banner.component';
 import { CategoryButtonComponent } from '../../shared/components/category-button/category-button.component';
 import { SearchFieldComponent } from '../../shared/components/search-field/search-field.component';
+import { Prato } from '../../shared/interfaces/prato';
+import { PratosService } from '../../shared/services/pratos.service';
 
 @Component({
   selector: 'app-home',
@@ -19,4 +21,23 @@ export class HomeComponent {
     "Pizza", "Hambúrguer", "Massa",
     "Pizza", "Hambúrguer", "Massa",
   ];
+
+  pratosService = inject(PratosService);
+
+  pratos: Prato[] = [];
+  pratosFiltrados: Prato[] = [];
+
+  ngOnInit(): void {
+    this.getPratos();
+    this.pratosFiltrados = this.getPratosFiltrados("");
+  }
+
+
+  getPratosFiltrados(nomeProduto: string){
+    return this.pratosService.findPrato(nomeProduto);
+  }
+
+  getPratos(){
+    return this.pratos = this.pratosService.getPratos();
+  }
 }
