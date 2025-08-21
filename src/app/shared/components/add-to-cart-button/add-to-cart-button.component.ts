8f4f5a1217f54aa56dd00c500c0b8720fd535d7e
Luana Paes <luanafranca598@gmail.com>
2025-08-21 20:41:32 -0300

    adiciona abertura do snackbar à função `addToCart´ para exibir mensagem sempre que um produto for adicionado ao carrinho

--- src/app/shared/components/add-to-cart-button/add-to-cart-button.component.ts
+++ src/app/shared/components/add-to-cart-button/add-to-cart-button.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Prato } from '../../interfaces/prato';
 import { CartService } from '../../services/cart.service';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AddPratoMessageSnackbarComponent } from '../add-prato-message-snackbar/add-prato-message-snackbar.component';
 @Component({
   selector: 'app-add-to-cart-button',
   standalone: true,
@@ -10,11 +11,18 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './add-to-cart-button.component.scss'
 })
 export class AddToCartButtonComponent {
+  constructor(private _snackBar: MatSnackBar) { }
+  
   @Input() prato!: Prato;
 
   cartService = inject(CartService);
 
   addToCart(){
     this.cartService.add(this.prato);
+    
+    this._snackBar.openFromComponent(AddPratoMessageSnackbarComponent, {
+      duration: 2 * 1000,
+      data: this.prato.nome
+    });
   }
 }
