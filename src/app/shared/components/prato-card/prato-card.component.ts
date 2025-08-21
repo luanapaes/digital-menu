ed461fa3a9902a0b74d3222259b90469fd40f0ff
Luana Paes <luanafranca598@gmail.com>
2025-08-21 18:30:43 -0300

    adiciona input fullPrato para envia-lo para o AddToCartButton

--- src/app/shared/components/prato-card/prato-card.component.ts
+++ src/app/shared/components/prato-card/prato-card.component.ts
@@ -1,10 +1,12 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { AddToCartButtonComponent } from "../add-to-cart-button/add-to-cart-button.component";
+import { Prato } from '../../interfaces/prato';
 
 @Component({
   selector: 'app-prato-card',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, AddToCartButtonComponent],
   templateUrl: './prato-card.component.html',
   styleUrl: './prato-card.component.scss'
 })
@@ -14,4 +16,5 @@ export class PratoCardComponent {
   @Input() preco: number = 0;
   @Input() pratoFoto: string = '';
   @Input() categoria: string = '';
+  @Input() fullPrato!: Prato;
 }
