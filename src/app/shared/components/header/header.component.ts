245f22fe072acef3a4ec8a3c626277bd7c11286c
Luana Paes <luanafranca598@gmail.com>
2025-08-21 21:58:48 -0300

    adiciona função `openCart()` para abrir o carrinho de compras com configuração para deixar o dialog responsivo em diferentes tamanhos de telas.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CartComponent } from '../cart/cart.component';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +11,13 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(public dialog: MatDialog, public breakpointObserver: BreakpointObserver) { } 
 
+  openCart(){
+    const isMobile = this.breakpointObserver.isMatched('(max-width: 600px)');
+
+    this.dialog.open(CartComponent, {
+      width: isMobile ? '90%' : '400px'
+    });
+  }
 }
