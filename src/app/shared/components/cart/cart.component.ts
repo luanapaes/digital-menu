064945b115631cfb1969ce587a06d4477961aec5
Luana Paes <luanafranca598@gmail.com>
2025-08-21 22:46:26 -0300

    adiciona lógica ao CartCompAdiciona lógica ao CartComponent
    - adiciona `loadPratos` que utiliza o `CartService` para trazer os pratos que estão armazenados no localstorage e alimentar o `pratosArray`
    
    - adiciona função `closeDialog` para fechar o carrinho
    
    - adiciona função `calcularTotal` que soma os preços dos pratos e alimenta a propriedade `total`
    
    - remove importações que não estavam sendo utilizadas

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
-import {
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogTitle,
-} from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialog, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { CartService } from '../../services/cart.service';
+import { Prato } from '../../interfaces/prato';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [MatButtonModule, MatDialogContent, MatDialogActions],
+  imports: [MatButtonModule, MatDialogContent, MatDialogActions, CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
+  cartService = inject(CartService);
+  pratosArray: Prato[] = [];
+  total: number = 0
+
+  constructor(public dialog: MatDialog) { } 
+  
+  ngOnInit(): void {
+    this.loadPratos();
+    this.calcularTotal();
+  }
+  
+  loadPratos(){
+    this.pratosArray = this.cartService.get();
+  }
+
+  closeCarrinho(){
+    this.dialog.closeAll();
+  }
+
+
+  calcularTotal(){
+    this.total = this.pratosArray.reduce((ac, n) => ac += n.preco, 0)
+  }
 
 }
