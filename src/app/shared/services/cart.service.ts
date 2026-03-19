811bbf1f5c51d1c871ae56c0c74beec46a9a96fc
Luana Paes <luanafranca598@gmail.com>
2026-03-19 10:20:16 -0300

    feat: adiciona funções - loadFromLocalStorage() que carrega os pratos ou retorna um array vazio; - saveToLocalStorage(); - verifyPrato() que recebe um prato e verifica se ele já existe no array, caso sim, ele incrementa quantidade, caso não, ele adiciona esse prato ao carrinho; e adiciona o construtor para inicializar o carrinho já com os dados do localstorage ao criar o service.

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -1,34 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Prato } from '../interfaces/prato';
+import { CustomPrato } from '../interfaces/custom-prato';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  pratosCarrinho: CustomPrato[] = [];
 
-  pratosCarrinho: Prato[] = [];
-
-  add(prato: Prato){
-    this.pratosCarrinho.push(prato)
-    return localStorage.setItem('pratos', JSON.stringify(this.pratosCarrinho))
+  constructor() {
+    this.loadFromLocalStorage();
   }
 
-  get(){
-    let arr = localStorage.getItem('pratos');
+  private loadFromLocalStorage() {
+    const arr = localStorage.getItem('pratos');
+    this.pratosCarrinho = arr ? JSON.parse(arr) : [];
+  }
 
-    if(arr){
-      return JSON.parse(arr);
-    } else {
-      console.error("Erro ao recuperar array do localStorage");
-      
-    }
+  private saveToLocalStorage() {
+    localStorage.setItem('pratos', JSON.stringify(this.pratosCarrinho));
+  }
+
+  add(prato: CustomPrato) {
+    this.pratosCarrinho.push(prato);
+    this.saveToLocalStorage();
+  }
+
+  get(): CustomPrato[] {
+    this.loadFromLocalStorage();
+    return this.pratosCarrinho;
   }
 
   removePrato(pratoName: string) {
-    let pratos: Prato[] | [] = JSON.parse(localStorage.getItem("pratos") || "[]");
+    this.pratosCarrinho = this.pratosCarrinho.filter(p => p.nome !== pratoName);
+    this.saveToLocalStorage();
+  }
 
-    pratos = pratos.filter((prato: any) => prato.nome !== pratoName);
+  //verifica se o prato já existe no carrinho para aumentar a quantidade ou adicionalo
+  verifyPrato(prato: CustomPrato) {
+    const existingPrato = this.pratosCarrinho.find(p => p.nome === prato.nome);
 
-    localStorage.setItem("pratos", JSON.stringify(pratos));
+    if (existingPrato && existingPrato.qtdPedido !== undefined) {
+      existingPrato.qtdPedido++;
+    } else {
+      const newPrato: CustomPrato = { ...prato, qtdPedido: prato.qtdPedido ?? 1 };
+      this.add(newPrato);
+    }
+    this.saveToLocalStorage();
   }
 }
